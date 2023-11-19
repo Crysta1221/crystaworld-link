d13e21d8c7e217d607077892b437066bf42365da
Crysta1221 <takuyakojim@outlook.jp>
2023-11-19 19:57:34 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,307 @@
+<template>
+<div class="bg-green-50 p-8 sm:p-16 min-h-screen">
+  <div class="flex flex-col sm:flex-row">
+    <div class="sm:w-2/5 lg:w-1/3">
+      <img src="https://avatars.githubusercontent.com/u/70198466?v=4" class="h-32 w-32 rounded-full mx-auto" />
+      <h1 class="font-bold text-3xl text-center my-6">
+        Crysta1221
+      </h1>
+      <p class="text-lg mt-4 -mb-0.5 font-semibold text-center my-6"><span class="under">&ensp;About&ensp;</span></p>
+      <p class="text-center text-base">
+        よわよわなフロントエンドエンジニア
+      </p>
+      <p class="text-lg mt-4 -mb-0.5 font-semibold text-center my-6"><span class="under">&ensp;Skill&ensp;</span></p>
+      <div class="flex space-x-1 justify-center items-center sm:flex-row flex-wrap mt-4 mb-4">
+        <div class="group relative w-max">
+        <Icon icon="skill-icons:html" height="32" width="32"/>
+        <span
+          class="pointer-events-none absolute -top-7 left-0 w-max rounded bg-gray-900 px-2 py-1 text-sm text-gray-50 opacity-0 shadow transition-opacity group-hover:opacity-100" 
+        >
+          HTML 
+        </span>
+      </div>
+      <div class="group relative w-max">
+        <Icon icon="skill-icons:javascript" height="32" width="32"/>
+        <span
+          class="pointer-events-none absolute -top-7 left-0 w-max rounded bg-gray-900 px-2 py-1 text-sm text-gray-50 opacity-0 shadow transition-opacity group-hover:opacity-100" 
+        >
+          JavaScript
+        </span>
+      </div>
+      <div class="group relative w-max">
+        <Icon icon="skill-icons:typescript" height="32" width="32"/>
+        <span
+          class="pointer-events-none absolute -top-7 left-0 w-max rounded bg-gray-900 px-2 py-1 text-sm text-gray-50 opacity-0 shadow transition-opacity group-hover:opacity-100" 
+        >
+          TypeScript
+        </span>
+      </div>
+      <div class="group relative w-max">
+        <Icon icon="skill-icons:vuejs-dark" height="32" width="32"/>
+        <span
+          class="pointer-events-none absolute -top-7 left-0 w-max rounded bg-gray-900 px-2 py-1 text-sm text-gray-50 opacity-0 shadow transition-opacity group-hover:opacity-100" 
+        >
+          Vue.js
+        </span>
+      </div>
+      <div class="group relative w-max">
+        <Icon icon="skill-icons:nuxtjs-dark" height="32" width="32"/>
+        <span
+          class="pointer-events-none absolute -top-7 left-0 w-max rounded bg-gray-900 px-2 py-1 text-sm text-gray-50 opacity-0 shadow transition-opacity group-hover:opacity-100" 
+        >
+          Nuxt.js
+        </span>
+      </div>
+      <div class="group relative w-max">
+        <Icon icon="skill-icons:expressjs-dark" height="32" width="32"/>
+        <span
+          class="pointer-events-none absolute -top-7 left-0 w-max rounded bg-gray-900 px-2 py-1 text-sm text-gray-50 opacity-0 shadow transition-opacity group-hover:opacity-100" 
+        >
+          Express.js
+        </span>
+      </div>
+      <div class="group relative w-max">
+        <Icon icon="skill-icons:discordbots" height="32" width="32"/>
+        <span
+          class="pointer-events-none absolute -top-7 left-0 w-max rounded bg-gray-900 px-2 py-1 text-sm text-gray-50 opacity-0 shadow transition-opacity group-hover:opacity-100" 
+        >
+          Discord.js
+        </span>
+      </div>
+      <div class="group relative w-max">
+        <Icon icon="skill-icons:tailwindcss-dark" height="32" width="32"/>
+        <span
+          class="pointer-events-none absolute -top-7 left-0 w-max rounded bg-gray-900 px-2 py-1 text-sm text-gray-50 opacity-0 shadow transition-opacity group-hover:opacity-100" 
+        >
+          Tailwind CSS
+        </span>
+      </div>
+      <div class="group relative w-max">
+        <Icon icon="skill-icons:linux-dark" height="32" width="32"/>
+        <span
+          class="pointer-events-none absolute -top-7 left-0 w-max rounded bg-gray-900 px-2 py-1 text-sm text-gray-50 opacity-0 shadow transition-opacity group-hover:opacity-100" 
+        >
+          Linux
+        </span>
+      </div>
+      <div class="group relative w-max">
+        <Icon icon="skill-icons:bash-dark" height="32" width="32"/>
+        <span
+          class="pointer-events-none absolute -top-7 left-0 w-max rounded bg-gray-900 px-2 py-1 text-sm text-gray-50 opacity-0 shadow transition-opacity group-hover:opacity-100" 
+        >
+          Shell
+        </span>
+      </div>
+      <div class="group relative w-max">
+        <Icon icon="skill-icons:docker" height="32" width="32"/>
+        <span
+          class="pointer-events-none absolute -top-7 left-0 w-max rounded bg-gray-900 px-2 py-1 text-sm text-gray-50 opacity-0 shadow transition-opacity group-hover:opacity-100" 
+        >
+          Docker
+        </span>
+      </div>
+      </div>
+    </div>
+    <div class="sm:w-3/5">
+      <!-- First Section -->
+      <p class="text-2xl font-bold">📌 Pinned</p>
+      <div class="flex sm:flex-row flex-col flex-wrap justify-start">
+        <div class="w-full sm:w-1/2 lg:w-1/3 p-4">
+          <a href="https://github.com/Crysta1221" target="_blank">
+            <div class="bg-gray-200 hover:bg-zinc-200 shadow-lg rounded-lg">
+            <div class="px-4 py-4">
+              <div class="w-12 h-12 rounded-lg shadow-sm border border-zinc-200 mb-2 grorup-hover:-translate-y-0.5 duration-200 hover:-translate-y-px mr-3 flex items-center justify-center" style="background-color: rgb(36, 41, 47);">
+                <Icon icon="simple-icons:github" class="p-1.5 text-4xl text-white" width="42" height="42"/>
+              </div>
+            </div>
+            <div class="p-4 -mt-8">
+              <h1 class="font-bold text-gray-900 text-xl">
+                Github
+              </h1>
+              <p class="text-gray-700">
+                @Crysta1221
+              </p>
+            </div>
+            </div>
+          </a>   
+        </div>
+        <div class="w-full sm:w-1/2 lg:w-1/3 p-4">
+          <a href="https://x.com/Cr1sta_dev" target="_blank">
+            <div class="bg-gray-200 hover:bg-zinc-200 shadow-lg rounded-lg">
+            <div class="px-4 py-4">
+              <div class="w-12 h-12 rounded-lg shadow-sm border border-zinc-200 mb-2 grorup-hover:-translate-y-0.5 duration-200 hover:-translate-y-px mr-3 flex items-center justify-center" style="background-color: rgb(29,161,242);">
+                <Icon icon="simple-icons:twitter" class="p-1.5 text-4xl text-white" width="38" height="38"/>
+              </div>
+            </div>
+            <div class="p-4 -mt-8">
+              <h1 class="font-bold text-gray-900 text-xl">
+                Twitter(𝕏)
+              </h1>
+              <p class="text-gray-700">
+                @Cr1sta_dev
+              </p>
+            </div>
+            </div>
+          </a>  
+        </div>
+        <div class="w-full sm:w-1/2 lg:w-1/3 p-4">
+          <div class="bg-gray-200 hover:bg-zinc-200 shadow-lg rounded-lg">
+            <div class="px-4 py-4">
+              <div class="w-12 h-12 rounded-lg shadow-sm border border-zinc-200 mb-2 grorup-hover:-translate-y-0.5 duration-200 hover:-translate-y-px mr-3 flex items-center justify-center" style="background-color: rgb(88,101,242);">
+                <Icon icon="simple-icons:discord" class="p-1.5 text-4xl text-white" width="42" height="42"/>
+              </div>
+            </div>
+            <div class="p-4 -mt-8">
+              <h1 class="font-bold text-gray-900 text-xl">
+                Discord
+              </h1>
+              <p class="text-gray-700">
+                403 Forbidden
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="w-full sm:w-1/2 lg:w-1/3 p-4">
+          <a href="https://misskey.io/@Crysta1221" target="_blank">
+            <div class="bg-gray-200 hover:bg-zinc-200 shadow-lg rounded-lg">
+            <div class="px-4 py-4">
+              <div class="w-12 h-12 rounded-lg shadow-sm border border-zinc-200 mb-2 grorup-hover:-translate-y-0.5 duration-200 hover:-translate-y-px mr-3 flex items-center justify-center" style="background-color: rgb(36, 41, 56);">
+                <Icon icon="skill-icons:misskey-dark" class="p-1.5 text-4xl text-white" width="42" height="42"/>
+              </div>
+            </div>
+            <div class="p-4 -mt-8">
+              <h1 class="font-bold text-gray-900 text-xl">
+                Misskey.io
+              </h1>
+              <p class="text-gray-700">
+                @Crysta1221
+              </p>
+            </div>
+            </div>
+          </a>
+        </div>
+        <div class="w-full sm:w-1/2 lg:w-1/3 p-4">
+          <a href="https://qiita.com/Crysta1221" target="_blank">
+            <div class="bg-gray-200 hover:bg-zinc-200 shadow-lg rounded-lg">
+            <div class="px-4 py-4">
+              <div class="w-12 h-12 rounded-lg shadow-sm border border-zinc-200 mb-2 grorup-hover:-translate-y-0.5 duration-200 hover:-translate-y-px mr-3 flex items-center justify-center" style="background-color: rgb(85,197,0);">
+                <Icon icon="simple-icons:qiita" class="p-1.5 text-4xl text-white" width="48" height="48"/>
+              </div>
+            </div>
+            <div class="p-4 -mt-8">
+              <h1 class="font-bold text-gray-900 text-xl">
+                Qiita
+              </h1>
+              <p class="text-gray-700">
+                @Crysta1221
+              </p>
+            </div>
+            </div>
+          </a>  
+        </div>
+        <div class="w-full sm:w-1/2 lg:w-1/3 p-4">
+          <div class="bg-gray-200 hover:bg-zinc-200 shadow-lg rounded-lg">
+            <div class="px-4 py-4">
+              <div class="w-12 h-12 rounded-lg shadow-sm border border-zinc-200 mb-2 grorup-hover:-translate-y-0.5 duration-200 hover:-translate-y-px mr-3 flex items-center justify-center" style="background-color: rgb(241,43,21);">
+                <Icon icon="simple-icons:riotgames" class="p-1.5 text-4xl text-white" width="40" height="40"/>
+              </div>
+            </div>
+            <div class="p-4 -mt-8">
+              <h1 class="font-bold text-gray-900 text-xl">
+                Riot Games
+              </h1>
+              <p class="text-gray-700">
+                Cr1sta#くりすた
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Second Section -->
+      <p class="text-2xl font-bold mt-2">💼 Working</p>  
+      <div class="flex sm:flex-row flex-col flex-wrap justify-start">
+        <div class="w-full sm:w-1/2 lg:w-1/3 p-4">
+          <a href="https://tatamiserver.com" target="_blank">
+            <div class="bg-gray-200 hover:bg-zinc-200 shadow-lg rounded-lg">
+            <div class="px-4 py-4">
+              <div class="w-12 h-12 rounded-lg shadow-sm border border-zinc-200 mb-2 grorup-hover:-translate-y-0.5 duration-200 hover:-translate-y-px mr-3 flex items-center justify-center" style="background-color: rgb(255,255,255);">
+                <img src="/tatami.png" class="p-1.5 text-4xl text-white" width="46" height="46"/>
+              </div>
+            </div>
+            <div class="p-4 -mt-8">
+              <h1 class="font-bold text-gray-900 text-xl">
+                畳サーバー
+              </h1>
+              <p class="text-gray-700">
+                Manager
+              </p>
+            </div>
+            </div>
+          </a> 
+        </div>
+      </div>
+      <p class="text-2xl font-bold mt-2">📞 Contact</p>  
+      <div class="flex sm:flex-row flex-col flex-wrap justify-start">
+        <div class="w-full sm:w-1/2 lg:w-1/3 p-4">
+          <a href="mailto:crystaworld1221@gmail.com">
+            <div class="bg-gray-200 hover:bg-zinc-200 shadow-lg rounded-lg">
+            <div class="px-4 py-4">
+              <div class="w-12 h-12 rounded-lg shadow-sm border border-zinc-200 mb-2 grorup-hover:-translate-y-0.5 duration-200 hover:-translate-y-px mr-3 flex items-center justify-center" style="background-color: rgb(255,255,255);">
+                <Icon icon="logos:google-gmail" class="p-1.5 text-4xl " width="46" height="46"/>
+              </div>
+            </div>
+            <div class="p-4 -mt-8">
+              <h1 class="font-bold text-gray-900 text-xl">
+                Email
+              </h1>
+              <p class="text-gray-700">
+                Click to Send Email
+              </p>
+            </div>
+            </div>
+          </a>   
+        </div>
+        <div class="w-full sm:w-1/2 lg:w-1/3 p-4">
+          <a href="https://x.com/Cr1sta_dev" target="_blank">
+            <div class="bg-gray-200 hover:bg-zinc-200 shadow-lg rounded-lg">
+            <div class="px-4 py-4">
+              <div class="w-12 h-12 rounded-lg shadow-sm border border-zinc-200 mb-2 grorup-hover:-translate-y-0.5 duration-200 hover:-translate-y-px mr-3 flex items-center justify-center" style="background-color: rgb(29,161,242);">
+                <Icon icon="simple-icons:twitter" class="p-1.5 text-4xl text-white" width="38" height="38"/>
+              </div>
+            </div>
+            <div class="p-4 -mt-8">
+              <h1 class="font-bold text-gray-900 text-xl">
+                Twitter(𝕏)
+              </h1>
+              <p class="text-gray-700">
+                Direct Mail
+              </p>
+            </div>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<Footer/>
+</template>
+
+<script setup>
+import { Icon } from '@iconify/vue';
+</script>
+
+<script>
+import Footer from './components/footer.vue';
+
+export default {
+  return:
+    Footer
+}
+</script>
+
+<style scoped>
+.under {
+  border-bottom: solid 3px #49d179;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import './assets/style/style.scss'
+import App from './App.vue'
+import '@fontsource/zen-kaku-gothic-new';
+
+createApp(App).mount('#app')
